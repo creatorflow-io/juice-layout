2d39f3d053a2c4efb9f9d3eb4cc363240aab873c
ductran <ductran.tb@gmail.com>
2024-03-22 10:35:48 +0700

    fix(test): UserProfileDialogComponent

--- projects/juice-js/layout/src/lib/components/user-profile-dialog/user-profile-dialog.component.spec.ts
+++ projects/juice-js/layout/src/lib/components/user-profile-dialog/user-profile-dialog.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserProfileDialogComponent } from './user-profile-dialog.component';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
-describe('UserProfileComponent', () => {
+describe('UserProfileDialogComponent', () => {
   let component: UserProfileDialogComponent;
   let fixture: ComponentFixture<UserProfileDialogComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [UserProfileDialogComponent]
+      declarations: [UserProfileDialogComponent],
+      imports: [
+        OAuthModule.forRoot(),
+        HttpClientTestingModule,
+        TranslateModule.forRoot()
+      ]
     });
     fixture = TestBed.createComponent(UserProfileDialogComponent);
     component = fixture.componentInstance;
